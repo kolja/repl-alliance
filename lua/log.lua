deca2da328f1ea490cba7786099854ea09ecbfe0
Kolja Wilcke <kolja@apfel.de>
2020-03-09 17:20:32 +0800

    properly resolve elisions

--- lua/log.lua
+++ lua/log.lua
@@ -5,11 +5,20 @@ function Log:new (o)
     local obj = o or {
         entries = {},
         index = {}, -- maps group_id to entry-index
-        bookmark = 0
+        bookmark = 0,
+        elisions = {}
     }
     setmetatable(obj, self)
     self.__index = self
     return obj
+end
+
+function Log:needs_refresh(id)
+    local entry = self.entries[id]
+    if not entry then return nil end
+
+    entry.meta.needs_refresh = true
+    return id
 end
 
 function Log:log(key, msg, group_id)
@@ -29,21 +38,109 @@ function Log:log(key, msg, group_id)
         entry = h.last(self.entries)
         id = #(self.entries)
     end
-    return id
+    entry.meta = {}
+    return self:needs_refresh(id)
 end
 
-function Log:flush()
-    local new_entries = {}
-    if #self.entries == self.bookmark then return nil end
-    if #self.entries == self.bookmark + 1 then
-        new_entries = {h.last(self.entries)}
-    else
-        new_entries = h.filter(function(v,i)
-            return i > self.bookmark
-        end, self.entries)
+function Log:print(buffer, action)
+
+    local replWin = repl:getReplWin()
+    if not replWin then return end
+
+    local entries = h.filter(function(entry,i)
+        local state = entry.meta.needs_refresh
+        entry.meta.needs_refresh = false
+        return state
+    end, self.entries)
+
+    local pr = function(entry, buffer, action)
+        local out = ""
+        local n = vim.api.nvim_buf_line_count(buffer)
+        for channel,val in pairs(entry) do
+            local ac = action[channel]
+            if ac then
+                for i, el in ipairs(val) do
+                    out = out .. ac(el)
+                end
+            end
+        end
+        if not h.isempty(string.gsub(out, "[%s\n]+", "")) then
+            out = vim.split(out, "\n")
+            local from = entry.meta.from or n
+            local to = entry.meta.to or (from + #out)
+            vim.api.nvim_buf_set_lines(buffer, from, to, false, out)
+            entry.meta.from = from
+            entry.meta.to = to
+        end
     end
-    self.bookmark = #self.entries
-    return new_entries
+
+    h.run_in_buffer( function(buffer)
+        for i,logentry in ipairs(entries) do
+            pr(logentry, buffer, action)
+        end
+    end, buffer)
+end
+
+function Log:register_elisions(obj)
+
+    if obj:is("tagged_literal") and obj.children[1]:is("elision") then
+        local action = obj:get({2, ":get"})
+        local key = action:get({2})
+        local log_id = obj.root.log_id
+        self:add_elision({
+            key = key:str(),
+            log_id = log_id,
+            action = action:str(),
+            resolve = function(o)
+                local spliced = obj:splice(o)
+                self:register_elisions(spliced)
+            end
+        })
+    elseif obj:len() > 0 then
+        for i,child in ipairs(obj.children) do
+            self:register_elisions(child)
+        end
+    end
+    return obj
+end
+
+function Log:get_elision(key)
+    return h.filter(function(el)
+        return el.key == key
+    end, self.elisions)[1]
+end
+
+function Log:add_elision(data)
+    local e = self:get_elision(data.key)
+    if e then
+        e = vim.tbl_extend("keep", e, data)
+    else
+        table.insert(self.elisions, data)
+        e = data
+    end
+    return e
+end
+
+function Log:remove_elision(key)
+    self.elisions = h.remove(function(el)
+        return el.key == key
+    end, self.elisions)
+    return self.elisions
+end
+
+function Log:debug()
+    local buffer = repl:buffer()
+    local pr = function(str)
+        local n = vim.api.nvim_buf_line_count(buffer)
+        vim.api.nvim_buf_set_lines(buffer, n, -1, false, vim.split(str,"\n"))
+    end
+    for i, logentry in ipairs(self.entries) do
+        pr("---"..i.."---")
+        pr(table.concat(vim.tbl_keys(logentry), ", "))
+        if logentry[":eval"] then
+            pr(vim.inspect(logentry[":eval"]))
+        end
+    end
 end
 
 function Log:last()
